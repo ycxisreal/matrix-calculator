e955bf124c35137b77a88927202c91e10f69e8e8
cx y <ycx1634770529@163.com>
2022-05-16 23:20:24 +0800

    添加注意事项

--- 项目v3矩阵计算器/main.cpp
+++ 项目v3矩阵计算器/main.cpp
@@ -17,6 +17,7 @@ int main()
 		char choice;
 		cout << "【1】添加矩阵A" << endl;
 		cout << "【0】退出计算器" << endl;
+		cout << "【p】了解程序^" << endl;
 		cin >> choice;
 		getchar();
 		switch (choice)
@@ -26,8 +27,16 @@ int main()
 			break;
 		case '0':
 			return 0;
+		case 'p':
+			cout << "====================================" << endl;
+			cout << "注意事项：" << endl;
+			cout << "    不要在需要输入数字的情况输入非数字字符，否则会出现无限循环的情况。" << endl;
+			cout << "    在使用过程中遇到未知bug请及时与作者联系，qq1634770529" << endl;
+			system("pause");
+			break;
 		default:
 			cout << "【请输入合适的选项哦】" << endl;
+			system("pause");
 			break;
 		}
 	}
